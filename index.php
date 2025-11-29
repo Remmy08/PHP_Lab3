365757f33545f03a9b041a89fa3c79eabe2005ad
Ilya <ilya.tys08@gmail.com>
2025-11-30 00:34:08 +0500

    Complete OOP geometry implementation with Point and Vector classes

--- index.php
+++ index.php
@@ -1,45 +1,28 @@
 <?php
 require 'vendor/autoload.php';
-use App\MagicClass;
+use App\Point;
+use App\Vector;
 
-echo "<h2>Демонстрация магических методов</h2>";
+// Создаем объекты
+$T1 = new Point(3, 4);
+$V1 = new Vector(3, 4);
+$V2 = new Vector(0, 0);
+$V3 = $V1->getPerpendicular();
 
-echo "<h3>1. Создание объекта (__construct):</h3>";
-$obj = new MagicClass();
+echo "Точка T1: {$T1}<br>";
+echo "Вектор V1: {$V1}<br>";
+echo "Вектор V2: {$V2}<br>";
+echo "Вектор V3: {$V3}<br><br>";
 
-echo "<h3>2. Обращение к несуществующему свойству (__get):</h3>";
-$test = $obj->undefinedProperty;
+echo "Длины векторов:<br>";
+echo "V1: " . number_format($V1->length(), 2) . "<br>";
+echo "V2: " . number_format($V2->length(), 2) . "<br>";
+echo "V3: " . number_format($V3->length(), 2) . "<br><br>";
 
-echo "<h3>3. Установка несуществующего свойства (__set):</h3>";
-$obj->newProperty = "value";
+echo "Перпендикулярность V1 и V3: " . 
+    ($V1->isPerpendicularTo($V3) ? "ДА" : "НЕТ") . "<br><br>";
 
-echo "<h3>4. Проверка существования свойства (__isset):</h3>";
-isset($obj->someProperty);
-
-echo "<h3>5. Удаление свойства (__unset):</h3>";
-unset($obj->someProperty);
-
-echo "<h3>6. Вызов несуществующего метода (__call):</h3>";
-$obj->undefinedMethod("param1", "param2");
-
-echo "<h3>7. Вызов несуществующего статического метода (__callStatic):</h3>";
-MagicClass::staticUndefinedMethod("static_param");
-
-echo "<h3>8. Преобразование объекта в строку (__toString):</h3>";
-echo $obj . "<br>";
-
-echo "<h3>9. Вызов объекта как функции (__invoke):</h3>";
-$obj("invoke_parameter");
-
-echo "<h3>10. Клонирование объекта (__clone):</h3>";
-$obj2 = clone $obj;
-
-echo "<h3>11. Сериализация (__sleep) и десериализация (__wakeup):</h3>";
-$serialized = serialize($obj);
-$unserialized = unserialize($serialized);
-
-echo "<h3>12. Вывод отладочной информации (__debugInfo):</h3>";
-var_dump($obj);
-
-echo "<h3>13. Уничтожение объектов (__destruct):</h3>";
-// __destruct вызовется автоматически при завершении скрипта
+echo "Перенос точки T1 на вектор V1:<br>";
+echo "Было: {$T1}<br>";
+$T1->move($V1->getX(), $V1->getY());
+echo "Стало: {$T1}<br>";
